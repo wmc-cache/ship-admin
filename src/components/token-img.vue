19c10bff232ea472f277ea37adc2b11b095cdadd
wmc <wu__mengcheng@163.com>
2021-05-14 20:49:40 +0800

    wmc

--- src/components/token-img.vue
+++ src/components/token-img.vue
@@ -1,39 +1,38 @@
 <template>
-  <img ref="img" /> 
+  <img ref="img">
 </template>
 <script>
-import {getToken}  from "../utils/auth"
+import { getToken } from '../utils/auth'
 export default {
-  name: "authImg",
+  name: 'AuthImg',
   props: {
     authSrc: {
       type: String,
       required: false,
-      default: "",
+      default: ''
     }
   },
-  mounted () {
-    let token = `Bearer ${getToken()}`
+  mounted() {
+    const token = `Bearer ${getToken()}`
     Object.defineProperty(Image.prototype, 'authsrc', {
       writable: true,
       enumerable: true,
       configurable: true
     })
-    let img = this.$refs.img
-    let request = new XMLHttpRequest();
-    request.responseType = 'blob';
-    request.open('get', this.authSrc, true);
-    request.setRequestHeader('Authorization', token);
+    const img = this.$refs.img
+    const request = new XMLHttpRequest()
+    request.responseType = 'blob'
+    request.open('get', this.authSrc, true)
+    request.setRequestHeader('Authorization', token)
     request.onreadystatechange = e => {
       if (request.readyState == XMLHttpRequest.DONE && request.status == 200) {
-            
-       img.src = URL.createObjectURL(request.response);
+       img.src = URL.createObjectURL(request.response)
         img.onload = () => {
-          URL.revokeObjectURL(img.src);
+          URL.revokeObjectURL(img.src)
         }
       }
-    };
-    request.send(null);
+    }
+    request.send(null)
   }
 }
 </script>
--- src/views/ship/upload.vue
+++ src/views/ship/upload.vue
@@ -1,62 +1,62 @@
 <template>
-	<div>
-		<!-- <img
+  <div>
+    <!-- <img
 			:src="`http://192.168.199.186:8009/union/img/${url}`"
 			alt="加载失败"
 		> -->
-		<token-img auth-src="https://images.pexels.com/photos/7682736/pexels-photo-7682736.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"></token-img>
-		<input
-			ref="input"
-			type="file"
-			@change="handleFileChange"
-		/>
-		<button @click="upload">上传文件</button>
-		<button @click="submit">提交</button>
-	</div>
+    <token-img auth-src="https://images.pexels.com/photos/7682736/pexels-photo-7682736.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
+    <input
+      ref="input"
+      type="file"
+      @change="handleFileChange"
+    >
+    <button @click="upload">上传文件</button>
+    <button @click="submit">提交</button>
+  </div>
 </template>
 
 <script>
-import axios from "axios";
-import { getToken } from "@/utils/auth";
-import { editDevice } from "../../api/ship";
-import tokenImg from '@/components/token-img.vue';
+import axios from 'axios'
+import { getToken } from '@/utils/auth'
+import { editDevice } from '../../api/ship'
+import tokenImg from '@/components/token-img.vue'
 export default {
   components: { tokenImg },
 	data() {
 		return {
 			urlList: [],
-			url: null,
-		};
+			url: null
+		}
 	},
 	methods: {
 		upload() {
-			this.$refs.input.click();
+			this.$refs.input.click()
 		},
 		handleFileChange(e) {
-			const currentTarget = e.target;
+			const currentTarget = e.target
 			if (currentTarget.files) {
-				const files = Array.from(currentTarget.files);
-				console.log("files", files);
-				const formData = new FormData();
-				formData.append("file", files[0]);
+				const files = Array.from(currentTarget.files)
+				console.log('files', files)
+				const formData = new FormData()
+				formData.append('file', files[0])
 				axios
-					.post("/admin/uploadFile", formData, {
+					.post('/admin/uploadFile', formData, {
 						headers: {
-							"Content-Type": "multipart/form-data",
-							Authorization: `Bearer ${getToken()}`,
-						},
+							'Content-Type': 'multipart/form-data',
+							Authorization: `Bearer ${getToken()}`
+						}
 					})
 					.then((res) => {
-						console.log(res.data.data.picName);
-						this.url = res.data.data.picName;
-					});
+						console.log(res.data.data.picName)
+						this.url = res.data.data.picName
+					})
 			}
 		},
 		submit() {
-			//editDevice()
-		},
-	},
-};
+			// editDevice()
+		}
+	}
+}
 </script>
 
 <style scoped>
